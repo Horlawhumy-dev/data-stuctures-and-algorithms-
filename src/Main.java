bc1c69d274895e3cd9813b89b821246914489900
Arafat Olayiwola <harof.dev@gmail.com>
2022-04-19 17:58:45 +0100

    Fixed Main

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {3,848,4090, 4429, 663};
-        List<Integer> arr = ElementGreaterThanAverage.findElementsGreaterThanAverage(array);
-        System.out.println(arr);
-        ReverseArrayOfIntegerValues res = new ReverseArrayOfIntegerValues();
-        res.setArrayOfInteger(array);
-        System.out.println(Arrays.toString(res.reverseArrayOfIntegers()));
+
     }
 }
