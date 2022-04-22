39e29ff785ad3e7e332b4f5c4b2d73bd11a5cede
Arafat Olayiwola <harof.dev@gmail.com>
2022-04-22 09:26:33 +0100

    Added some solution and fixed files

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 import Random.*;
 
+import java.io.*;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+//        BufferedReader R = new BufferedReader(new InputStreamReader(System.in));
+//        File file = new File("C:\\Users\\HP\\Desktop\\DSA\\src\\data.txt");
+//        System.out.print("Input your name: ");
+//        String name = R.readLine();
+//        OutputStream str = new FileOutputStream(file);
+//        str.write(Integer.parseInt(name));
+//        System.out.println("Your name is: " + name);
+//        str.close();
+        int[] arr = {3, 4, 2, 1, 3};
+//        ReverseStringOfArray r = new ReverseStringOfArray(s);
+//        System.out.println(r.reverseArrayOfStrings(s));
+        System.out.println(FindDuplicateInteger.getDuplicate(arr));
 
     }
 }
--- src/Random/FindDuplicateInteger.java
+++ src/Random/FindDuplicateInteger.java
@@ -1,7 +1,25 @@
 package Random;
 
-public class FindDuplicateInteger {
+import java.util.HashSet;
 
+public class FindDuplicateInteger {
+    // fair solution
+    // Time complexity => O(n)
+    // Space complexity => O(n)
+    public static boolean getDuplicate(int[] array) {
+        HashSet<Integer> set = new HashSet<>(); // set does not contain duplicate
+        for (int element: array) {
+            if (set.contains(element)) {
+                return true;
+            }
+            set.add(element);
+        }
+        return false;
+    }
+
+//     Brute force solution
+//    Time Complexity => O(n*n)
+//     space complexity => O(1)
     public static boolean containDuplicates(int[] array) {
         for(int i = 0; i < array.length; ++i) {
             for(int j = i + 1; j < array.length - 1; ++j) {
