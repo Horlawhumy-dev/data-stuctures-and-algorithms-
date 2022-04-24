7861122668af370d060ef2c6b00035d3e3d025e6
Arafat Olayiwola <harof.dev@gmail.com>
2022-04-24 18:52:34 +0100

    Added solution to doubly linked list

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import Array.RemoveDuplicatesInSortedArray;
 import Array.SquaresOfSortedArrays;
 import LinkedIist.CLL;
+import LinkedIist.DLL;
 
 import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args)  {
-
+        DLL<Integer> list = new DLL<>();
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(9);
+        list.insertFirst(11);
+        list.insertLast(90);
+        list.insertLast(91);
+        list.printCurrentTail();
+        list.printCurrentHead();
+        list.displayLL();
     }
 }
