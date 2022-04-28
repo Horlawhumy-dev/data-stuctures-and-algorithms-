9d7fa2a1a9c1aea2f6a2f2b7f62a6fd488aeff43
Arafat Olayiwola <harof.dev@gmail.com>
2022-04-28 18:37:30 +0100

    Added solution to circular doubly linkedlist

---
+++ src/LinkedIist/CircularDoublyLL.java
@@ -0,0 +1,193 @@
+package LinkedIist;
+
+public class CircularDoublyLL<T extends Comparable<T>>{
+    private Node<T> head = null;
+    private Node<T> tail = null;
+    private int size = 0;
+
+    public void insertNode(T value) {
+        Node<T> node = new Node<> (value);
+        if (head == null) {
+            head = node;
+            tail = head;
+            tail.next = node;
+            tail.prev = node;
+            size++;
+            return;
+        }
+        tail.setNext(node);
+        node.setPrev(tail);
+        tail = node;
+        tail.setNext(head);
+        head.setPrev(tail);
+        size++;
+    }
+
+    public void insertFirst(T value) {
+        Node<T> node = new Node<>(value);
+        node.setNext(head);
+        head = node;
+        if (tail ==  null) {
+            tail = head;
+            //tail.setNext(null);
+            return;
+        }
+        //node.setPrev(null);
+        size++;
+    }
+
+
+    public void insertLast(T value){
+        Node<T> node = new Node<>(value);
+        node.setNext(null);
+
+        if (head == null) {
+            node.setPrev(null);
+            head = node;
+            tail = head;
+            return;
+        }
+        tail.next = node;
+        node.setPrev(tail);
+        tail = node;
+        size++;
+    }
+
+    public int insertNodeAtIndex(T value, int index) {
+        // guard before anything
+        if (index > size || index < 0) {
+            return -1;
+        }
+
+        if (index == size) {
+            insertNode(value);
+            return size;
+        }
+//        Node<T> node = new Node<>(value);
+//        Node<T> prevNode = getPrevNode(index); // get previous node before the position of the new node
+//        prevNode.next.next.setPrev(node);
+//        prevNode.setNext(node); //setting the next node of the previous node to the new node
+//        node.setNext(prevNode.next.next); // setting the node at the specific index as the next one to the new node
+//        node.setPrev(prevNode);
+//        size++;
+        return size;
+    }
+
+    public void deleteNode(String type) {
+        if (type.equalsIgnoreCase("first")) {
+            Node<T> node = head;
+            head = node.next;
+            size--;
+        }
+        if (type.equalsIgnoreCase("last")) {
+           tail = getPrevNode(size-1);
+           tail.setNext(head);
+           head.prev = tail;
+           size--;
+        }
+
+    }
+
+    public Node<T> deleteNodeAtAnyIndex(int index) {
+        Node<T> prevNode = getPrevNode(index);
+        Node<T> node = prevNode.next;
+        prevNode.setNext(prevNode.next.next);
+        size--;
+        return node;
+    }
+
+    public T findNodeElement(int index){
+        Node<T> prevNode = getPrevNode(index);
+        return prevNode.next.getData();
+    }
+
+    // this is getting the previous node to the index
+    public Node<T> getPrevNode(int index) {
+        Node<T> current = head;
+        for (int i = 0; i < size; i++) {
+            if (i == index-1) {
+                return current;
+            }
+            current = current.getNext();
+        }
+        return current;
+    }
+
+    public void printCurrentTail() {
+        System.out.println(tail.getData());
+
+    }
+
+    public void printCurrentHead() {
+        System.out.println(head.getData());
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void displayLL() {
+        Node<T> current = head;
+//        Node<T> last = current;
+        while(current.next != head) {
+            System.out.print(current.getData() + " -> ");
+//            last = current;
+            current = current.getNext();
+
+        }
+        System.out.println(" END");
+//
+//        System.out.println("-------------------------");
+//        while (last != null) {
+//            System.out.print(last.getData() + " <= ");
+//            last = last.next;
+//        }
+//        System.out.println(" START");
+
+    }
+
+    public static class Node<T extends Comparable<T>> {
+        protected T data;
+        private Node<T> next;
+        private Node<T> prev;
+
+        protected Node(T data) {
+            this.data = data;
+//            setNext(null);
+//            setPrev(null);
+        }
+
+
+        protected void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        protected Node<T> getNext() {
+            return this.next;
+        }
+
+        protected void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
+
+        protected Node<T> getPrev() {
+            return this.prev;
+        }
+
+
+        protected void setData(T data) {
+            this.data = data;
+        }
+
+        protected T getData() {
+            return data;
+        }
+
+        public String toString() {
+            return String.valueOf(data);
+        }
+
+    }
+
+}
--- src/LinkedIist/Main.java
+++ src/LinkedIist/Main.java
@@ -2,24 +2,28 @@ package LinkedIist;
 
 public class Main {
     public static void main(String[] args) {
-        LL<Integer> list = new LL<>();
-        list.insertFirst(9);
-        list.insertFirst(8);
-        list.insertFirst(6);
-        list.insertFirst(4);
-        list.insertFirst(2);
-        list.insertFirst(1);
-        list.display();
+        CircularDoublyLL<Integer> list = new CircularDoublyLL<>();
+        list.insertNode(9);
+        list.insertNode(8);
+        list.insertNode(6);
+        list.insertNode(4);
+        list.insertNode(2);
+        list.insertNode(1);
+        list.printCurrentTail();
+        list.printCurrentHead();
+        list.displayLL();
+        System.out.println( list.insertNodeAtIndex(8, 5));
+        list.displayLL();
 
-        LL<Integer> list2 = new LL<>();
-        list2.insertFirst(17);
-        list2.insertFirst(15);
-        list2.insertFirst(13);
-        list2.insertFirst(11);
-        list2.display();
-
-        LL<Integer> list3 = new LL<>();
-        LL<Integer> result = list3.mergeSortedLL(list, list2);
-        result.display();
+//        LL<Integer> list2 = new LL<>();
+//        list2.insertFirst(17);
+//        list2.insertFirst(15);
+//        list2.insertFirst(13);
+//        list2.insertFirst(11);
+//        list2.display();
+//
+//        LL<Integer> list3 = new LL<>();
+//        LL<Integer> result = list3.mergeSortedLL(list, list2);
+//        result.display();
     }
 }
