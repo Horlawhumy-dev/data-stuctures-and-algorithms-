224cf313aad82afa95a0199b93a97276a966f855
Arafat Olayiwola <harof.dev@gmail.com>
2022-05-02 18:15:03 +0100

    Added solutions to sort colors using recursion

--- src/Recursion/Main.java
+++ src/Recursion/Main.java
@@ -5,8 +5,14 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 4, 3, 5, 5};
+        int[] array = {1, 2, 4, 0, 6, 5};
+//        0, 1, 2, 4, 6, 5
 //        System.out.println(FindTarget.findTargetIndices(array, 5));
-        Pattern.drawTriangle(0, 0);
+        SortColors.sortColors(array);
+//        int mid = array.length / 2;
+//        int[] left = Arrays.copyOfRange(array, 0, mid);
+//        int[] right = Arrays.copyOfRange(array, mid, array.length);
+//        System.out.println(Arrays.toString(right));
     }
+
 }
