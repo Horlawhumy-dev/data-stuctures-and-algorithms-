f1ca591bbe01d80a9a4f8fc294b5bbbfaa3675c2
Arafat Olayiwola <harof.dev@gmail.com>
2022-05-03 03:13:52 +0100

    Added solutions to sort colors using merge sort algo

--- src/Recursion/Main.java
+++ src/Recursion/Main.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 4, 0, 6, 5};
+        int[] array = {1, 2, 0, 0, 5, 5};
 //        0, 1, 2, 4, 6, 5
 //        System.out.println(FindTarget.findTargetIndices(array, 5));
         SortColors.sortColors(array);
+//        System.out.println(Arrays.toString(array));
 //        int mid = array.length / 2;
 //        int[] left = Arrays.copyOfRange(array, 0, mid);
 //        int[] right = Arrays.copyOfRange(array, mid, array.length);
--- src/Recursion/SortColors.java
+++ src/Recursion/SortColors.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 
 public class SortColors {
     /*
-        Time Complexity => O(n)
-        Space Complexity => O(1)
+        Time Complexity => O(n*logn)
+        Space Complexity => O(n)
      */
     public static void sortColors(int[] colors) {
         if (colors.length == 0) {
             return;
         }
-
-        System.out.println(Arrays.toString(helper2(colors)));
+        System.out.println(Arrays.toString(mergeSort(colors)));
     }
 
+    // Selection sort using recursion
     private static void helper(int[] colors, int end, int start, int maxIndex) {
         //base case to avoid infinite recursion
         if (end == 0) {
@@ -25,70 +25,67 @@ public class SortColors {
         if (start <= end) {
             if (colors[start] > colors[maxIndex]) {
                 // this is to get maximum element index
-                helper(colors, end, start+1, start);
-            }else {
+                helper(colors, end, start + 1, start);
+            } else {
                 // this is to get maximum element index
-                helper(colors, end, start+1, maxIndex);
+                helper(colors, end, start + 1, maxIndex);
             }
-        }else {
+        } else {
             swap(colors, maxIndex, end);
             // swapping the maximum element with the last element at every recursive calls while start index is less than end.
-            helper(colors, end-1, 0, 0);
+            helper(colors, end - 1, 0, 0);
         }
 
     }
 
-    private static int[] helper2(int[] nums) {
+    private static void swap(int[] colors, int maxIndex, int end) {
+        int temp = colors[maxIndex];
+        colors[maxIndex] = colors[end];
+        colors[end] = temp;
+    }
+
+    // Using Divide and Conquer Algorithm through merge sort
+    private static int[] mergeSort(int[] colors) {
+        if (colors.length == 1) {
+            return colors;
+        }
+
+        int mid = colors.length / 2;
+        int[] left = mergeSort(Arrays.copyOfRange(colors, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(colors, mid, colors.length));
+        return merge(left, right);
+    }
+
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
         int i = 0;
         int j = 0;
         int k = 0;
-        int mid = nums.length / 2;
-        int[] left = Arrays.copyOfRange(nums, 0, mid);
-        int[] right = Arrays.copyOfRange(nums, mid, nums.length);
-        int[] result = new  int[left.length + right.length];
-        helper3(i, j, k, left, right,result);
-        return result;
-    }
 
-    private static void helper3(int i, int j, int k, int[] left, int[] right, int[] result) {
-
-        if (i < left.length && j < right.length) {
-            if (left[i] <= right[j] ) {
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
                 result[k] = left[i];
                 i++;
-            }else {
+            } else {
                 result[k] = right[j];
                 j++;
             }
             k++;
-            helper3(i, j, k, left, right,result);
         }
 
-        helper4(i, j, k, left, right,result);
-    }
-
-    private static void helper4(int i, int j, int k, int[] left, int[] right, int[] result) {
-        if (i == left.length || j == right.length) {
-            return;
-        }
-
-        if (i < left.length) {
+        while (i < left.length) {
             result[k] = left[i];
             i++;
             k++;
-            helper4(i, j, k, left, right,result);
         }
-        if (j < right.length) {
-             result[k] = right[j];
-             j++;
-             k++;
-             helper4(i, j, k, left, right,result);
-        }
-    }
 
-    private static void swap(int[] colors, int maxIndex, int end) {
-         int temp = colors[maxIndex];
-         colors[maxIndex] = colors[end];
-          colors[end] = temp;
+        while (j < right.length) {
+            result[k] = right[j];
+            j++;
+            k++;
+        }
+
+        return result;
     }
 }
